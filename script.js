db84db41987bc6c916fa2925ef87f8aa72176939
HarishGoud8019 <23bk1a05j5@stpetershyd.com>
2025-09-14 12:55:43 +0530

    css,htmlcomplete

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("welcome to spotify");
+let audioElement = new Audio('1.mp3')
+let songs = [
+ {songName:"salam-e-ishq",filePath:"songs/1.mp3",coverPath: "covers/1.jpg"},
+ {songName:"salam-e-ishq",filePath:"songs/1.mp3",coverPath: "covers/1.jpg"},
+ {songName:"salam-e-ishq",filePath:"songs/1.mp3",coverPath: "covers/1.jpg"},
+ {songName:"salam-e-ishq",filePath:"songs/1.mp3",coverPath: "covers/1.jpg"},
+ {songName:"salam-e-ishq",filePath:"songs/1.mp3",coverPath: "covers/1.jpg"},
+ {songName:"salam-e-ishq",filePath:"songs/1.mp3",coverPath: "covers/1.jpg"},
+ {songName:"salam-e-ishq",filePath:"songs/1.mp3",coverPath: "covers/1.jpg"},
+ {songName:"salam-e-ishq",filePath:"songs/1.mp3",coverPath: "covers/1.jpg"},
+]
